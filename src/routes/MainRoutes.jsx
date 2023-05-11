8619be7f0463a571403aa63dd12bdf89fa5d5bc6
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2023-05-11 14:46:05 +0600

    added add

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import CollectionList from "../components/Products/CollectionList";
 import { Route, Routes } from "react-router-dom";
 import AuthPAge from "../components/Products/AuthPage";
+import AddProduct from "../components/Products/AddProduct";
 
 const MainRoutes = () => {
   const COLLECTION_ROUTES = [
     { link: "/", element: <CollectionList />, id: 1 },
     { link: "/admin", element: <AuthPAge />, id: 2 },
+    { link: "/add", element: <AddProduct />, id: 2 },
   ];
   return (
     <>
