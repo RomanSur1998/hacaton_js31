14e6659e52556a7313ab604bdda6c19a7f735f6b
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2023-05-12 14:25:04 +0600

    added type

--- src/context/CollectionContextProvider.js
+++ src/context/CollectionContextProvider.js
@@ -1,16 +1,29 @@
 import axios from "axios";
 import React, { createContext, useState } from "react";
+import { JSON_API_CLOTHES, JSON_API_OBJECT } from "../helpers/consts";
+import { useNavigate } from "react-router-dom";
 export const collectionContext = createContext();
 
 const CollectionContextProvider = ({ children }) => {
+  const navigate = useNavigate();
   const [collection, setCollection] = useState([]);
+
   async function getCards() {
-    let { data } = await axios("http://localhost:8000/1");
+    let { data } = await axios(JSON_API_CLOTHES);
 
     setCollection(data);
   }
 
-  const values = { getCards, collection };
+  const addCard = async (newProduct) => {
+    await axios.post(JSON_API_CLOTHES, newProduct);
+    navigate("/");
+  };
+
+  const deleteProduct = async (id) => {
+    await axios.delete(`${JSON_API_CLOTHESs}/${id}`);
+    getCards();
+  };
+  const values = { getCards, collection, addCard };
   return (
     <collectionContext.Provider value={values}>
       {children}
