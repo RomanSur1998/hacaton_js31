d355403993f18a57b461d84bf5e07ccb437be724
Yrysbek-Joroev <yrysjoro@gmail.com>
2023-05-12 11:44:47 +0600

    local

--- src/context/CollectionContextProvider.js
+++ src/context/CollectionContextProvider.js
@@ -3,11 +3,12 @@ import React, { createContext } from "react";
 export const collectionContext = createContext();
 const CollectionContextProvider = ({ children }) => {
   async function getCards() {
-    let res = await axios("  http://localhost:8000/1");
+    let res = await axios("http://localhost:8000/1");
     console.log(res);
   }
   getCards();
-  return <collectionContext.Provider>{children}</collectionContext.Provider>;
+  const values = {}
+  return <collectionContext.Provider value={values}>{children}</collectionContext.Provider>;
 };
 
 export default CollectionContextProvider;
