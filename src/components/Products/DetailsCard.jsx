778eed8b379bad81ad625809a090055d6fb6f8cf
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2023-05-16 13:45:51 +0600

    save changes color

--- src/components/Products/DetailsCard.jsx
+++ src/components/Products/DetailsCard.jsx
@@ -3,10 +3,13 @@ import { useNavigate, useParams } from "react-router-dom";
 import { collectionContext } from "../../context/CollectionContextProvider";
 import "../Products/styles/DetailsCard.css";
 import { cartContext } from "../../context/CartContextProvider";
+import favor from "../Images/icons8-favorites-128.png";
+import { favorContext } from "../../context/FavorContextProvider";
 const DetailsCard = () => {
   const { id } = useParams();
   const { oneCard, getCardtDetails } = useContext(collectionContext);
   const { addProductToCart } = useContext(cartContext);
+  const { addProductToFavor } = useContext(favorContext);
   const [card, setCard] = useState(oneCard);
   const navigate = useNavigate();
 
@@ -20,28 +23,45 @@ const DetailsCard = () => {
 
   return (
     <div className="containerDetails">
+      <h2>Details</h2>
       <div>
         <img src={card.image_1} alt="" />
       </div>
       <div className="sizePAram"> SIZE:{card.size}</div>
       <p>{card.decr}</p>
-
-      <button
-        onClick={() => {
-          addProductToCart(card);
-          navigate("/");
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
         }}
       >
-        Add to Cart
-      </button>
-
-      <button
-        onClick={() => {
-          navigate(`/order/${id}`);
-        }}
-      >
-        Buy Now{" "}
-      </button>
+        <img
+          onClick={() => {
+            addProductToFavor(card);
+          }}
+          src={favor}
+          alt=""
+          style={{ width: "35px", margin: "0" }}
+        />
+        <button
+          style={{ width: "160px" }}
+          onClick={() => {
+            addProductToCart(card);
+            navigate("/");
+          }}
+        >
+          Add to Cart
+        </button>
+        <button
+          onClick={() => {
+            navigate(`/order/${id}`);
+          }}
+          style={{ width: "160px" }}
+        >
+          Buy Now{" "}
+        </button>
+      </div>
     </div>
   );
 };
