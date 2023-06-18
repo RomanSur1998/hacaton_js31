d85e5be0f0d29e3e1f49845c5f0c179fcf830727
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2023-06-18 15:46:21 +0600

    added block public

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,4 +1,4 @@
-export const JSON_API_OBJECT = "http://localhost:8000/1";
+// export const JSON_API_OBJECT = "http://localhost:8000/1";
 export const JSON_API_CLOTHES = "http://localhost:8000/clothes";
 // export const JSON_API_CLOTHES = "https://json-server-sigma-hazel.vercel.app/";
 export const ADMIN = "admin@gmail.com";
